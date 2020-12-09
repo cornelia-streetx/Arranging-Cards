a0e4c374360bf50d360356c6f79130601512d8c2
srishtimishra08 <67187064+srishtimishra08@users.noreply.github.com>
2020-12-09 19:34:01 +0530

    JS Code for Arranging Cards Exercise

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-var i,j;
-var answer = document.getElementById("result");
+let i,j;
+let temp;
+
 function swap(arr, index1, index2){
-  var temp = arr[index1];
+  temp = arr[index1];
   arr[index1] = arr[index2];
   arr[index2] = temp;
 }
 
-function swapCards(arr){
-  for (i = 0; i < arr.length - 1; i++){
-    for (j = 0; j < arr.length - 1; j++){
-      if(arr[j] > arr[j+1]){
-        swap(arr, j , j+1);
-      }
+function arrange(arr){
+  for(i = 0; i < arr.length - 1; i++){
+  for(j = 0; j < arr.length - 1; j++){
+    if(arr[j] > arr[j+1]){
+      swap(arr, j , j+1);
     }
+  }
   }
   return arr;
 }
-answer.innerHTML=swapCards(["Home", 11 , 7 , 4 , 1 , 6 , 13 , 10 , 8 , 9]);
+
+result.innerHTML(arrange(["Home", 376, 465, 3, 46, 9]));
